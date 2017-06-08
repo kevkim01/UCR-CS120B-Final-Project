88a273daf58ac7e4687be015849d296751440508
kevkim01 <kkim068@ucr.edu>
2017-06-07 20:21:33 -0700

    added pawn shooting functionality

--- pieces.h
+++ pieces.h
@@ -19,6 +19,7 @@ void Draw_enemy();
 void Move_enemy();
 
 void Set_up_player();
+unsigned char Check_player();
 void Clear_player();
 void Draw_player();
 void Move_player(unsigned char direction);
@@ -29,8 +30,28 @@ void Draw_boss();
 void Move_boss();
 
 void Set_up_boss_shot();
+unsigned char Check_boss();
 void Clear_boss_shot();
 void Draw_boss_shot();
 unsigned char Move_boss_shot();
 
+void Set_up_pawn_shot1();
+void Set_up_pawn_shot2();
+void Set_up_pawn_shot3();
+
+void Clear_pawn_shot1();
+void Clear_pawn_shot2();
+void Clear_pawn_shot3();
+
+void Draw_pawn_shot1();
+void Draw_pawn_shot2();
+void Draw_pawn_shot3();
+
+unsigned char Move_pawn_shot1();
+unsigned char Move_pawn_shot2();
+unsigned char Move_pawn_shot3();
+
+unsigned char Check_for_fire1();
+unsigned char Check_for_fire2();
+unsigned char Check_for_fire3();
 #endif
